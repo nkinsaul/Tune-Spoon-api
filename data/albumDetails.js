9ad2437dad71eab5549864dfcc218d68f2e80c33
Natalie Kinsaul <n.kinsaul@gmail.com>
2023-02-18 13:17:56 -0800

    Add album information

--- data/albumDetails.js
+++ data/albumDetails.js
@@ -18,15 +18,290 @@ const albumDetails = [
       'Smoke',
       'Billions'
     ],
-    length: '45:25',
+    length: 2725,
     description: `Desire, I Want to Turn Into You is the fourth studio album by Caroline Polachek, and second under her given name. It was released on February 14, 2023 by Sony Music, The Orchard and Polachek's imprint Perpetual Novice.
 
     It was supported by the singles "Bunny Is a Rider", "Billions", "Sunset", "Welcome to My Island" and "Blood and Butter". The cover art was shot by Aidan Zamiri.
     
     After releasing Pang in late 2019, Polachek was set to tour the record, but was cut short by the COVID-19 pandemic in March 2020. Polachek stayed in London on begun work on Desire, I Want to Turn Into You with close collaborator Danny L Harle. She considers the album to be a major partnership with Harle, as the record has only a few other collaborators in the mix. She continued work on the album until mid-2021, where she briefly relocated to Barcelona alongside Harle and new collaborator Sega Bodega.`,
-    producer: 'Sony Music',
-    releaseDate: '2/14/2023',
+    producer: 'Caroline Polachek',
+    releaseDate: '02 Feb 2023, 17:22',
     genre: ['sunset', 'pop', 'art pop', 'progressive pop']
+  },
+
+  {
+    id: 2,
+    title: 'Sun Machine',
+    artist: 'Rubblebucket',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/c0b4a053cd548b420ba984a8caf84eb3.jpg#c0b4a053cd548b420ba984a8caf84eb3',
+    trackList: [
+      'What Life Is',
+      'Donna',
+      'Lemonade',
+      'Party Like Your Heart Hurts',
+      'Fruity',
+      'Formless and New',
+      'AURATALK',
+      'Annihilation Song',
+      'Vantalk',
+      'Inner Cry',
+      "Sunlit Sparks (I Won't Break You)",
+      'Hurtalk',
+      'Habit Creature'
+    ],
+    length: 2476,
+    description: 'In summer 2015, after finishing a year of intense touring, Rubblebucket/s Kalmia Traver and Alex Toth began the process of bringing their next record to life. As an experiment, Kalmia asked Alex (her longtime romantic partner) to move out while they worked on the album, then accepted the marriage proposal he made during a recording session just a month later. Although Alex soon moved back in, their 11-year relationship ended when the two chose to /consciously uncouple/ the following spring—a decision they honored by ceremoniously giving each other matching triangular daisy tattoos (a nod to the title track from Rubblebucket/s 2010 EP). But despite all the sadness brought on by their breakup, Kalmia and Alex kept on writing and recording together, ultimately creating Rubblebucket/s most transcendent album to date.',
+    producer: 'Rubblebucket',
+    genre: [
+      'personal favourites',
+      'rock',
+      'psychedelic',
+      'banned',
+      'indie pop'
+    ]
+  },
+
+  {
+    id: 3,
+    title: 'Unlimited Love',
+    artist: 'Red Hot Chili Peppers',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/07f19170333eb7221e6486b32d39da49.jpg#07f19170333eb7221e6486b32d39da49',
+    trackList: [
+      'Black Summer',
+      'Here Ever After',
+      'Aquatic Mouth Dance',
+      'Not the One',
+      'Poster Child',
+      'The Great Apes',
+      "It's Only Natural",
+      "She's a Lover",
+      'These Are the Ways',
+      "Whatchu Thinkin'",
+      'Bastards of Light',
+      'White Braids & Pillow Chair',
+      'One Way Traffic',
+      'Veronica',
+      "Let 'Em Cry",
+      'The Heavy Wing',
+      'Tangelo'
+    ],
+    length: 4383,
+    producer: 'Rick Rubin',
+    genre: [ 'rock', 'funk rock', 'funk', 'alternative rock' ]
+  },
+
+  {
+    id: 4,
+    title: SOS,
+    artist: SZA,
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/b2cfb5bdf137f4d6293565205965750f.jpg#b2cfb5bdf137f4d6293565205965750f',
+    trackList: [
+      'SOS',
+      'Kill Bill',
+      'Seek & Destroy',
+      'Low',
+      'Love Language',
+      'Blind',
+      'Used feat. Don Toliver',
+      'Snooze',
+      'Notice Me',
+      'Gone Girl',
+      'Smoking on My Ex Pack',
+      'Ghost in the Machine feat. Phoebe Bridgers',
+      'F2F',
+      'Nobody Gets Me',
+      'Conceited',
+      'Special',
+      'Too Late',
+      'Far',
+      'Shirt',
+      'Open Arms feat. Travis Scott',
+      'I Hate U',
+      'Good Days',
+      'Forgiveless feat. Ol/ Dirty Bastard'
+    ],
+    length: 4070,
+    description: `SOS is the second studio album by American singer SZA, released on December 9, 2022, via Top Dawg Entertainment and RCA Records. The album features guest appearances from Don Toliver, Phoebe Bridgers, Travis Scott, and the late Ol' Dirty Bastard.
+
+    SOS is primarily an R&B album with influences from hip hop and pop. The album samples sound references from soul, gospel, jazz and melodic rap. The sound of the album was described as "a varied palette", combining "surf rock" and "grunge" elements, alongside "her beloved lo-fi beats". In Complex, she described the album's sound: "I have no idea what it sounds like to anybody else. I really don't know. It's so bizarre. It's weird that I can't put my finger on it. It's a little bit of everything." She added that some parts of the song had an "aggressive" sound whereas others were soft or balladic. During the Rolling Stone interview, she and Punch spoke in length about the album's composition. The inspiration in question was eclectic, drawing from jazz, alternative rock, "traditional" R&B, country, and hip hop. About the wide range of genres incorporated into the album, SZA said she did not care if her production choices made the album sound incohesive, because to her, "if you sound like you, your shit/s going to be cohesive. Period." Punch commented: "It's a new chapter. She's not scared to try certain things now."
+    
+    SOS has received widespread critical acclaim upon its release. At Metacritic, which assigns a normalized rating out of 100 based on reviews from critics, the album received a score of 90 out of 100, based on 20 reviews, indicating "universal acclaim.`,
+    producer: 'Aire Atlantica',
+    releaseDate: '12/9/2022',
+    genre: ['soul', 'hip hop', 'rnb', 'r&b']
+  },
+
+  {
+    id: 5,
+    title: 'Earth Worship',
+    artist: 'Rubblebucket',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/5511ebd31c06a2baa22e56d4b79ecd12.jpg#5511ebd31c06a2baa22e56d4b79ecd12',
+    trackList: [
+      'Earth Worship Intro',
+      'Earth Worship',
+      'Morning In The Sun',
+      'Rain Rain Nature Rain',
+      'Geometry',
+      'Cherry Blossom',
+      'Melt Through The Floors',
+      'Zeros As Round As The World',
+      'Sexual Revolution',
+      'Sweet Spot',
+      'Truth In The Air',
+      'Mockingbird',
+      'Too Much Feeling'
+    ],
+    length: 2792,
+    description: 'Released October 21st, 2022 on Grand Jury Music',
+    producer: 'Grand Jury Music',
+    releaseDate: '10 Oct 2022, 16:32',
+    genre: ['indie pop', 'indie', 'indie rock', 'rock', 'american']
+  },
+
+  {
+    id: 6,
+    title: 'Choose Your Weapon',
+    artist: 'Hiatus Kaiyote',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/648cd00cee4d489a04a190aa20112a4f.jpg#648cd00cee4d489a04a190aa20112a4f',
+    trackList: [
+      'Choose Your Weapon',
+      'Shaolin Monk Motherfunk',
+      'Laputa',
+      'Creations Part One',
+      'Borderline With My Atoms',
+      'Breathing Underwater',
+      'Cicada',
+      'Swamp Thing',
+      'Fingerprints',
+      'Jekyll',
+      'Prince Minikid',
+      'Atari',
+      'By Fire',
+      'Creations Part Two',
+      'The Lung',
+      'Only Time All the Time: Making Friends with Studio Owl',
+      'Molasses',
+      'Building a Ladder'
+    ],
+    length: 4167,
+    description: `Choose Your Weapon is the second studio album by Australian neo-soul quartet Hiatus Kaiyote, first released in 1 May 2015 through Flying Buddha[2] and Sony Masterworks. Following the release of the band's debut album Tawk Tomahawk (2012) and their first Grammy nomination, Hiatus Kaiyote went on to tour throughout 2014.[3][4] Over the span of two years, recording sessions took place in Australia and the United States.
+
+    Choose Your Weapon is a neo soul album that takes influence from a variety of genres including jazz, soul, R&B, progressive rock, West African funk, and samba and Latin grooves. Thematically, the album's songs touch upon topics such as the supernatural, technology and death. All members of the band contributed lyrics and production, while Salaam Remi, the founder of Flying Buddha, served as the album's executive producer.
+    
+    Upon release, the album was met with universal acclaim from music critics, who praised the album's production and musical structure, and was named as one of the year’s best albums. The album saw some commercial success and debuted at #22 on the Australian albums chart. The song "Breathing Underwater" was nominated for Best R&B Performance at the 58th Grammy Awards.`,
+    producer: 'Salaam Remi (exec) and Hiatus Kaiyote',
+    genre: [ '2015', 'funk', 'nu jazz', 'acid jazz', 'neo-soul' ]
+  },
+
+  {
+    id: 7,
+    title: 'Drunk',
+    artist: 'Thundercat',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/17311ac4702bbc6245e9ee2958630c8f.jpg#17311ac4702bbc6245e9ee2958630c8f',
+    trackList: [
+      'Rabbot Ho',
+      'Captain Stupido',
+      'Uh Uh',
+      'Bus in These Streets',
+      "A Fan's Mail (Tron Song Suite II)",
+      'Lava Lamp',
+      'Jethro',
+      'Day & Night',
+      'Show You the Way',
+      'Walk on By',
+      'Blackkk',
+      'Tokyo',
+      "Jameel's Space Ride",
+      'Friend Zone',
+      'Them Changes',
+      "Where I'm Going",
+      'Drink Dat',
+      'Inferno',
+      'I Am Crazy',
+      '3AM',
+      'Drunk',
+      'The Turn Down',
+      'DUI'
+    ],
+    length: 2719,
+    description: `Drunk is the third studio album by American musician Thundercat. It was released on February 24, 2017 by Brainfeeder. It features guest appearances from Kenny Loggins, Michael McDonald, Kendrick Lamar, Wiz Khalifa, Mac Miller, and Pharrell. It is his first studio album in nearly four years, his last studio album being Apocalypse. Drunk received positive reviews from music critics. A ChopNotSlop remix from OG Ron C, DJ Candlestick, & The Chopstars entitled 'Drank' was released as a special edition purple vinyl record. <a href="https://www.last.fm/music/Thundercat/Drunk">Read more on Last.fm</a>.`,
+    producer: ['Flying Lotus', 'Sounwave', 'Thundercat'],
+    releaseDate: '08 Aug 2020, 13:40',
+    genre: [ 'jazz fusion', 'funk', '2015', 'jazz', 'soul' ]
+  },
+
+  {
+    id: 8,
+    title: 'Special',
+    artist: 'Lizzo',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/aa0fa19a3c3cdff70672d238c9861358.jpg#aa0fa19a3c3cdff70672d238c9861358',
+    trackList: [
+      'The Sign',
+      'About Damn Time',
+      'Grrrls',
+      '2 Be Loved (Am I Ready)',
+      'I Love You Bitch',
+      'Special',
+      'Break Up Twice',
+      'Everybody’s Gay',
+      'Naked',
+      'Birthday Girl',
+      'If You Love Me',
+      'Coldplay'
+    ],
+    length: 1172,
+    description: `"Special" is the fourth studio album by American singer and rapper Lizzo. It was released through Nice Life and Atlantic Records on July 15, 2022. It was preceded by the lead single "About Damn Time", released on April 14, 2022. This is her second studio album under a major label, after 2019's "Cuz I Love You". In October 2020, Lizzo announced that her fourth studio album was nearing completion, saying she had "a few more songs to write". In January 2021, American singer SZA confirmed to have heard new material from Lizzo. In August 2021, Lizzo announced "Rumors", her first <a href="https://www.last.fm/music/Lizzo/Special">Read more on Last.fm</a>.`,
+    label: 'Ricky Reed',
+    releaseDate: '07 Jul 2022, 18:26',
+    genre: ['funk', 'pop', 'dance', 'r&b']
+  },
+
+  {
+    id: 9,
+    title: 'Schvitz',
+    artist: 'Vulfpeck',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/ba61a9b60778fb9ed2e8c209f19055d1.jpg#ba61a9b60778fb9ed2e8c209f19055d1',
+    trackList: [
+      'Sauna',
+      'Earworm',
+      'New Guru',
+      "All That's Left Of Me Is You",
+      'Simple Step',
+      'In Heaven',
+      'Serve Somebody',
+      'Romanian Drinking Song',
+      'What Did You Mean By Love?',
+      'Miracle'
+    ],
+    length: 2067,
+    description: `Schvitz is the sixth studio album by American funk band Vulfpeck, released on 30 December 2022 through their own label, Vulf Records. After a brief hiatus, the band returned to recording together for Cory Wong's 2022 album Wong's Cafe, and performed together for the first time in three years at the Levitate Music & Arts Festival in July 2022. It is their first album to feature Wong, Antwaun Stanley and Joey Dosik as prominent performers on all tracks. Announced in November, the album was preceded by six singles.`,
+    producer: 'Vulf Records (independent)',
+    genre: [ 'soul', 'funk', 'novelty', 'jazz funk', 'jazz pop' ]
+  },
+
+  {
+    id: 10,
+    title: 'Back to Black',
+    artist: 'Amy Winehouse',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/bc4e31504f5f47adb31f36aa0889be45.jpg#bc4e31504f5f47adb31f36aa0889be45',
+    trackList: [
+      'Rehab',
+      "You Know I'm No Good",
+      'Me & Mr Jones',
+      'Just Friends',
+      'Back to Black',
+      'Love Is a Losing Game',
+      'Tears Dry on Their Own',
+      'Wake Up Alone',
+      'Some Unholy War',
+      'He Can Only Hold Her',
+      'Addicted'
+    ],
+    length: 1183,
+    description: `Back to Black is the second and final studio album by English singer-songwriter Amy Winehouse, released on October 27, 2006 by Island Records. Winehouse predominantly based the album on her tumultuous relationship with then-ex-boyfriend and future husband Blake Fielder-Civil, who temporarily left her to pursue his previous ex-girlfriend. Their short-lived separation spurred her to create an album that explores themes of guilt, grief, infidelity, heartbreak and trauma in a relationship. Influenced by the pop and soul music of 1960s girl groups, Winehouse collaborated with producers Salaam Remi and Mark Ronson, along with Sharon Jones' band The Dap-Kings, to assist her.`,
+    producer: 'Mark Ronson',
+    releaseDate: '06 Jun 2022, 20:50',
+    genre: [ 'soul', 'jazz', 'female vocalists', 'british' ]
   }
 ]
 
--- data/albums.js
+++ data/albums.js
@@ -4,6 +4,69 @@ const albums = [
     title: 'Desire, I Want to Turn Into You', 
     artist: 'Caroline Polachek',
     image: 'https://www.last.fm/music/Caroline+Polachek/Desire,+I+Want+to+Turn+Into+You/+images/97ae5e7ade492179bec606df66c41f35'
+  },
+
+  {
+    id: 2,
+    title: 'Sun Machine',
+    artist: 'Rubblebucket',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/c0b4a053cd548b420ba984a8caf84eb3.jpg#c0b4a053cd548b420ba984a8caf84eb3',
+  },
+
+  {
+    id: 3,
+    title: 'Unlimited Love',
+    artist: 'Red Hot Chili Peppers',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/07f19170333eb7221e6486b32d39da49.jpg#07f19170333eb7221e6486b32d39da49',
+  },
+
+  {
+    id: 4,
+    title: SOS,
+    artist: SZA,
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/b2cfb5bdf137f4d6293565205965750f.jpg#b2cfb5bdf137f4d6293565205965750f'
+  },
+
+  {
+    id: 5,
+    title: 'Earth Worship',
+    artist: 'Rubblebucket',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/5511ebd31c06a2baa22e56d4b79ecd12.jpg#5511ebd31c06a2baa22e56d4b79ecd12'
+  },
+
+  {
+    id: 6,
+    title: 'Choose Your Weapon',
+    artist: 'Hiatus Kaiyote',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/648cd00cee4d489a04a190aa20112a4f.jpg#648cd00cee4d489a04a190aa20112a4f'
+  },
+
+  {
+    id: 7,
+    title: 'Drunk',
+    artist: 'Thundercat',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/17311ac4702bbc6245e9ee2958630c8f.jpg#17311ac4702bbc6245e9ee2958630c8f'
+  },
+
+  {
+    id: 8,
+    title: 'Special',
+    artist: 'Lizzo',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/aa0fa19a3c3cdff70672d238c9861358.jpg#aa0fa19a3c3cdff70672d238c9861358'
+  },
+
+  {
+    id: 9,
+    title: 'Schvitz',
+    artist: 'Vulfpeck',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/ba61a9b60778fb9ed2e8c209f19055d1.jpg#ba61a9b60778fb9ed2e8c209f19055d1'
+  },
+
+  {
+    id: 10,
+    title: 'Back to Black',
+    artist: 'Amy Winehouse',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/bc4e31504f5f47adb31f36aa0889be45.jpg#bc4e31504f5f47adb31f36aa0889be45'
   }
 ]
 
