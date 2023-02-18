6a0a106aac5f90194b7ec9ea4dd7a2738607b9d3
Adam Hughes <adamjordanhughes@gmail.com>
2023-02-18 16:11:22 -0500

    Adams album data

--- data/albums.js
+++ data/albums.js
@@ -1,9 +1,70 @@
 const albums = [
-  { 
+  {
     id: 1,
-    title: 'Desire, I Want to Turn Into You', 
+    title: 'Desire, I Want to Turn Into You',
     artist: 'Caroline Polachek',
     image: 'https://www.last.fm/music/Caroline+Polachek/Desire,+I+Want+to+Turn+Into+You/+images/97ae5e7ade492179bec606df66c41f35'
+  },
+
+  {
+    id: 31,
+    title: 'Is There Anybody Out There?',
+    artist: 'A Great Big World',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/6f0e3a2d54b943f8c356977218f89520.jpg#6f0e3a2d54b943f8c356977218f89520',
+  },
+  {
+    id: 32,
+    title: 'Forget the World',
+    artist: 'Afrojack',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/de12f907fdbb42b1c232cc56c600c6c3.jpg#de12f907fdbb42b1c232cc56c600c6c3',
+  },
+  {
+    id: 33,
+    title: 'Best Day Ever',
+    artist: 'Mac Miller',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/98b0f62da13145c9a27102abf874f07e.jpg#98b0f62da13145c9a27102abf874f07e',
+  },
+  {
+    id: 34,
+    title: 'Dizzy Up the Girl',
+    artist: 'The Goo Goo Dolls',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/b5637551f8256bd31d895ad9db01d5d0.jpg#b5637551f8256bd31d895ad9db01d5d0',
+  },
+  {
+    id: 35,
+    title: 'Living Proof',
+    artist: 'State Champs',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/ff78ad66e21a26922e7288c860386acc.jpg#ff78ad66e21a26922e7288c860386acc',
+  },
+  {
+    id: 36,
+    title: 'Stories',
+    artist: 'Avicii',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/719ec212f89411b93ce528283ea8c007.jpg#719ec212f89411b93ce528283ea8c007',
+  },
+  {
+    id: 37,
+    title: 'the highs.',
+    artist: 'mike.',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/4ced1d846cfe63891fd638642e590965.jpg#4ced1d846cfe63891fd638642e590965',
+  },
+  {
+    id: 38,
+    title: 'Blacc Hollywood',
+    artist: 'Wiz Khalifa',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/59f3420cebb24680c6f4abb41b002599.jpg#59f3420cebb24680c6f4abb41b002599',
+  },
+  {
+    id: 39,
+    title: 'Brothers in Arms',
+    artist: 'Dire Straits',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/d3394952d369468ec64a7d5582c013cc.jpg#d3394952d369468ec64a7d5582c013cc',
+  },
+  {
+    id: 40,
+    title: 'Come Back Road',
+    artist: 'Logan Mize',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/5b9176350456f44bc34953f3e17d8a1b.jpg#5b9176350456f44bc34953f3e17d8a1b',
   }
 ]
 
