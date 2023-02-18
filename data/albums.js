1a319ebc211c4e391ddb835e928a0d8a0128c0de
Gus Deribeaux <Gusderibeaux@gmail.com>
2023-02-18 16:33:50 -0500

    Complete album list

--- data/albums.js
+++ data/albums.js
@@ -5,60 +5,6 @@ const albums = [
     artist: 'Caroline Polachek',
     image: 'https://www.last.fm/music/Caroline+Polachek/Desire,+I+Want+to+Turn+Into+You/+images/97ae5e7ade492179bec606df66c41f35'
   },
-  { 
-    id: 11,
-    title: 'Breaking the Balls of History',
-    artist: 'Quasi',
-    image: 'https://lastfm.freetls.fastly.net/i/u/60x60/2f113cd61ef5fea9730d49692c254b84.jpg'
-  },
-  { 
-    id: 12,
-    title: 'An Innocent Man',
-    artist: 'Billy Joel',
-    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/a12d150c799c416daef11949670485c8.jpg#a12d150c799c416daef11949670485c8',
-  },
-  { 
-    id: 14,
-    title: 'Frontiers',
-    artist: 'Journey',
-    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/a3bd61d82f22400abe781387166e7aa3.jpg#a3bd61d82f22400abe781387166e7aa3',
-  },
-  { 
-    id: 15,
-    title: 'Honky Chateau',
-    artist: 'Elton John',
-    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/600850c3207f8a27ff77e49d91f78c1b.jpg#600850c3207f8a27ff77e49d91f78c1b',
-  },
-  { 
-    id: 16,
-    title: 'Abbey Road',
-    artist: 'The Beatles',
-    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/af251669a48a4bafb448e1f6c0de01be.jpg#af251669a48a4bafb448e1f6c0de01be',
-  },
-  { 
-    id: 17,
-    title: 'Toys in the Attic',
-    artist: 'Aerosmith',
-    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/f9e9632ca1da6116f2b6e5f70ae9e7df.jpg#f9e9632ca1da6116f2b6e5f70ae9e7df',
-  },
-  { 
-    id: 18,
-    title: 'Reggatta de Blanc',
-    artist: 'The Police',
-    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/8135a6fa56f78744a678c5a0468417f0.jpg#8135a6fa56f78744a678c5a0468417f0',
-  },
-  { 
-    id: 19,
-    title: 'Face Value',
-    artist: 'Phil Collins',
-    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/565a36701b2ce857c1a581a769ceee62.jpg#565a36701b2ce857c1a581a769ceee62',
-  },
-  { 
-    id: 20,
-    title: 'Thriller',
-    artist: 'Michael Jackson',
-    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/e5f40ae3767cf5b6184776f97e52b8ca.jpg#e5f40ae3767cf5b6184776f97e52b8ca',
-  },
   {
     id: 2,
     title: 'Sun Machine',
@@ -114,14 +60,66 @@ const albums = [
     artist: 'Vulfpeck',
     image: 'https://lastfm.freetls.fastly.net/i/u/770x0/ba61a9b60778fb9ed2e8c209f19055d1.jpg#ba61a9b60778fb9ed2e8c209f19055d1'
   },
-
   {
     id: 10,
     title: 'Back to Black',
     artist: 'Amy Winehouse',
     image: 'https://lastfm.freetls.fastly.net/i/u/770x0/bc4e31504f5f47adb31f36aa0889be45.jpg#bc4e31504f5f47adb31f36aa0889be45'
   },
-  
+  { 
+    id: 11,
+    title: 'Breaking the Balls of History',
+    artist: 'Quasi',
+    image: 'https://lastfm.freetls.fastly.net/i/u/60x60/2f113cd61ef5fea9730d49692c254b84.jpg'
+  },
+  { 
+    id: 12,
+    title: 'An Innocent Man',
+    artist: 'Billy Joel',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/a12d150c799c416daef11949670485c8.jpg#a12d150c799c416daef11949670485c8',
+  },
+  { 
+    id: 14,
+    title: 'Frontiers',
+    artist: 'Journey',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/a3bd61d82f22400abe781387166e7aa3.jpg#a3bd61d82f22400abe781387166e7aa3',
+  },
+  { 
+    id: 15,
+    title: 'Honky Chateau',
+    artist: 'Elton John',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/600850c3207f8a27ff77e49d91f78c1b.jpg#600850c3207f8a27ff77e49d91f78c1b',
+  },
+  { 
+    id: 16,
+    title: 'Abbey Road',
+    artist: 'The Beatles',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/af251669a48a4bafb448e1f6c0de01be.jpg#af251669a48a4bafb448e1f6c0de01be',
+  },
+  { 
+    id: 17,
+    title: 'Toys in the Attic',
+    artist: 'Aerosmith',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/f9e9632ca1da6116f2b6e5f70ae9e7df.jpg#f9e9632ca1da6116f2b6e5f70ae9e7df',
+  },
+  { 
+    id: 18,
+    title: 'Reggatta de Blanc',
+    artist: 'The Police',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/8135a6fa56f78744a678c5a0468417f0.jpg#8135a6fa56f78744a678c5a0468417f0',
+  },
+  { 
+    id: 19,
+    title: 'Face Value',
+    artist: 'Phil Collins',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/565a36701b2ce857c1a581a769ceee62.jpg#565a36701b2ce857c1a581a769ceee62',
+  },
+  { 
+    id: 20,
+    title: 'Thriller',
+    artist: 'Michael Jackson',
+    image: 'https://lastfm.freetls.fastly.net/i/u/770x0/e5f40ae3767cf5b6184776f97e52b8ca.jpg#e5f40ae3767cf5b6184776f97e52b8ca',
+  },
   {
     id: 21,
     title: 'Worlds',
